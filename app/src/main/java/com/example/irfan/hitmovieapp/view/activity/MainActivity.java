893812c66292545b22858760ce4812ef302b5939
irfan <irfan.irawan@gits.co.id>
2016-05-05 03:11:49 +0700

    first commit

---
+++ app/src/main/java/com/example/irfan/hitmovieapp/view/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.irfan.hitmovieapp.view.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.irfan.hitmovieapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/irfan/hitmovieapp/widget/CustomTextView.java
@@ -0,0 +1,55 @@
+package com.example.irfan.hitmovieapp.widget;
+
+import android.annotation.TargetApi;
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Typeface;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.widget.TextView;
+
+import com.example.irfan.hitmovieapp.R;
+
+/**
+ * Created by irfan on 05/05/16.
+ */
+public class CustomTextView extends TextView {
+    public static final String ANDROID_SCHEMA = "http://schemas.android.com/apk/res/android";
+
+    public CustomTextView(Context context) {
+        super(context);
+        applyFont(null);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        applyFont(attrs);
+    }
+
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        applyFont(attrs);
+    }
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    public CustomTextView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        applyFont(attrs);
+    }
+
+    private void applyFont(AttributeSet attrs){
+        if (attrs != null) {
+            TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.CustomTextView);
+            String mFontName = a.getString(R.styleable.CustomTextView_font);
+            try {
+                if (mFontName != null) {
+                    Typeface mTypeface = Typeface.createFromAsset(getContext().getAssets(), "fonts/" + mFontName);
+                    setTypeface(mTypeface);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            a.recycle();
+        }
+    }
+}
